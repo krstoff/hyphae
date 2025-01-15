17848fab3dc70f0c83f199a1ec3aba377a0f7d62
Kristoffer Semelka <ksemelka@gmail.com>
2025-01-15 12:29:40 -0800

    Fix

--- packer/install-deps.sh
+++ packer/install-deps.sh
@@ -3,7 +3,7 @@ doas -u root su <<HERE
 apk add containerd containerd-openrc
 # debugging
 apk add nano cri-tools dhclient
-cat >/etc/modules-load.d/containerd.conf "overlay"
+echo >/etc/modules-load.d/containerd.conf "overlay"
 modprobe overlay
 rc-update add containerd default
 rc-service containerd start
