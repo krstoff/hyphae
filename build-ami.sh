b2bcbcc8c5e62c785fbd0bca362ccab00bcb353c
Kristoffer Semelka <ksemelka@gmail.com>
2025-01-21 15:10:29 -0800

    upload agent

--- build-ami.sh
+++ build-ami.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -euo pipefail
+pushd agent/
+cargo build --release
+popd
 pushd packer/
 packer build --var "commit-id=$(git rev-parse --short HEAD)" .
 popd
--- packer/install-deps.sh
+++ packer/install-deps.sh
@@ -7,3 +7,5 @@ systemctl enable containerd
 mv /tmp/10-containers.network /etc/systemd/network/
 mkdir /etc/iproute2
 mv /tmp/rt_tables /etc/iproute2/
+chmod +x /tmp/hyphae-agent
+mv /tmp/agent /usr/local/bin/hyphae-agent
